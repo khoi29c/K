839a2236641551479a0261435377c5dcab0e709f
khoi29c <bome15523@gmail.com>
2025-05-05 18:07:53 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
-print("Hello World")
+if game.PlaceId == 2753915549 then
+    print("[SWIFT] Script chạy trong Blox Fruits thành công!")
+else
+    warn("[SWIFT] Đây không phải là Blox Fruits.")
+end
