5b38a9b956ac9fd3a2c6e4d8e45a6dbbbae716ae
khoi29c <bome15523@gmail.com>
2025-05-05 18:21:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
-local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/khoi29c/K/main/main.lua"))()
-end)
+print("[main.lua] Hello từ GitHub!")
 
-if success then
-    print("[SWIFT] Script đã chạy thành công!")
+-- Kiểm tra game có phải Blox Fruits
+if game.PlaceId == 2753915549 then
+    print("[main.lua] Đây là Blox Fruits")
 else
-    warn("[SWIFT] Lỗi khi chạy script từ GitHub: " .. tostring(err))
+    warn("[main.lua] Không phải Blox Fruits!")
 end
